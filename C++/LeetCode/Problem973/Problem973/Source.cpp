ac6155db32c48417d4b846a385b1095c49230139
SangHeon-Cha <chashkim@gmail.com>
2019-04-21 06:05:04 +0900

    973번 문제
    https://leetcode.com/problems/k-closest-points-to-origin/

---
+++ C++/LeetCode/Problem973/Problem973/Source.cpp
@@ -0,0 +1,16 @@
+/*
+https://leetcode.com/problems/k-closest-points-to-origin/
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+void main()
+{
+	cout << "hello" << endl;
+
+	int i = 0;
+	cin >> i;
+}
